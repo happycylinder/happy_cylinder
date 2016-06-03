bea692314872191bfe596b96d0160faccdd6b005
happycylinder <aphexpusherofcanada@gmail.com>
2016-06-03 00:46:37 -0500

    beginning structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # happy_cylinder
+
+A Cylinder is a popular representation of a database
+
+I'm here to make your cylinder happy
---
+++ config.py
@@ -0,0 +1,5 @@
+# This is the default config file. For local configuration you don't want to check in, 
+# create a "local_config.py"
+
+# Put the URI of the database you want to work with here
+MYSQL_URI = ""
